e1e51f347109502bb28dce7be8fade7030a56206
azuki-osiruko <azukitofucake@outlook.jp>
2026-09-15 04:31:41 +0900

    Update リンク先ページ読み込み指定.js

--- リンク先ページ読み込み指定.js
+++ リンク先ページ読み込み指定.js
@@ -1,26 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const links = document.querySelectorAll("a");
-  links.forEach(link => {
-    const url = link.getAttribute("href");
-    // 外部リンクは新しいタブで開く
-    if (url && url.startsWith("http")) {
-      link.target = "_blank";
+	const links = document.querySelectorAll("a");
+	links.forEach(link => {
+		
+		const url = link.getAttribute("href");
+		
+		// popover トリガーはフェードアウト処理を無視
+		if (link.hasAttribute("popovertarget")) return;
+		
+		// 外部リンクは新しいタブで開く
+		if (url && url.startsWith("http")) {
+			link.target = "_blank";
 			link.rel = "noopener noreferrer";
-      return; // 外部リンクはフェードアウトしない
-    }
-    // 内部リンクだけフェードアウト
-    link.addEventListener("click", e => {
-      if (!url || url.startsWith("#")) return;
-      e.preventDefault();
-      document.body.classList.add("fade-out");
-      setTimeout(() => {window.location = url;}, 500);
-    });
-  });
+			return; // 外部リンクはフェードアウトしない
+		}
+		
+		// 内部リンクだけフェードアウト
+		link.addEventListener("click", e => {
+			if (!url || url.startsWith("#")) return;
+			e.preventDefault();
+			document.body.classList.add("fade-out");
+			setTimeout(() => {window.location = url;}, 500);
+		});
+	});
 });
+
 window.addEventListener("pageshow", function(event) {
-  if (event.persisted) {
-    // 戻る時（bfcache復帰）は fadeIn を無効化
-    document.body.style.animation = "none";
-    document.body.style.opacity = "1";
-  }
+	if (event.persisted) {
+		// 戻る時（bfcache復帰）は fadeIn を無効化
+		document.body.style.animation = "none";
+		document.body.style.opacity = "1";
+	}
 });
